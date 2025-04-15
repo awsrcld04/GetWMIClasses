c3e1be56507f505a02fa75786f214a495f92721f
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-15 16:28:39 -0400

    Initial commit.

---
+++ GetWMIClasses.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+namespace GetWMIClasses
+{
+    class GetWMIClasses
+    {
+        static void Main(string[] args)
+        {
+            ManagementScope scope = new ManagementScope(@"\\.\root\cimv2");
+            scope.Connect();
+
+            using (ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher(scope, new ObjectQuery("SELECT * FROM meta_class")))
+            {
+                searcher.Options.EnumerateDeep = false;
+                using (ManagementObjectCollection results = searcher.Get())
+                {
+                    foreach (ManagementObject result in results)
+                    {
+                        Console.WriteLine(result.ClassPath);
+                    };
+                };
+            };
+        } 
+    }
+}
